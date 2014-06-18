1570d9370c10627d6c9a06d32bd0934b292319d8
vzvu3k6k <vzvu3k6k@gmail.com>
2014-06-19 01:55:33 +0900

    Comment out temporarily

--- _tools/deploy-gh-pages.sh
+++ _tools/deploy-gh-pages.sh
@@ -2,11 +2,11 @@
 
 declare currentDir=$(cd $(dirname $0);pwd)
 
-if [ "$TRAVIS_BRANCH" != "master" ] ; then
-    echo "This is a ${TRAVIS_BRANCH} branch. No deployment will be done.";
-    ${currentDir}/deploy-preview-html.sh
-    exit 0;
-fi
+# if [ "$TRAVIS_BRANCH" != "master" ] ; then
+#     echo "This is a ${TRAVIS_BRANCH} branch. No deployment will be done.";
+#     ${currentDir}/deploy-preview-html.sh
+#     exit 0;
+# fi
 
 if [ $TRAVIS_PULL_REQUEST != 'false' ]; then
     echo "This is a pull request. No deployment will be done.";
