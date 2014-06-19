b9a495c1e93493dfd8594d4a846156c2158f0714
travis-ci <travis@travis-ci.org>
2014-06-19 11:50:50 +0000

    Travis build 5

---
+++ Ch2_HowToWrite/embed/embed-promise-all-timer.js
@@ -0,0 +1,20 @@
+// `delay`ミリ秒後にresolveする
+function timerPromisefy(delay) {
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            resolve(delay);
+        }, delay);
+    });
+}
+var startDate = Date.now();
+// 全てがresolveされたら終了
+Promise.all([
+    timerPromisefy(1),
+    timerPromisefy(32),
+    timerPromisefy(64),
+    timerPromisefy(128)
+]).then(function (values) {
+    console.log(Date.now() - startDate + 'ms');
+    // 約32ms
+    console.log(values);    // [1, 2, 4, 8, 16, 32]
+});
